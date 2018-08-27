1798a4bc31f382b0bdae865f8463a1de2bda560b
arohigupta <argu2081@colorado.edu>
2018-08-27 11:20:15 -0700

    adding bootstarp.sh

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+yum -y update
+yum -y install kernel-3.10.0-862.3.2.el7.x86_64 
+cat /etc/redhat-release 
+yum -y install epel-release 
+yum -y install git ansible-2.4.2.0
+yum install -y net-tools
+yum install -y nano
+yum install screen
+echo "10.87.5.6  contrail-node3.contrail-node contrail-node3” >> /etc/hosts
+cat /etc/hosts
+echo "nameserver 172.29.131.60" >> /etc/resolv.conf
+echo "search jnpr.net juniper.net" >> /etc/resolv.conf
